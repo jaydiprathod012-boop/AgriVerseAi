1126919bfe9d87f9f47ed82012177e60bb3b246a
jaydiprathod012-boop <jaydiprathod012@gmail.com>
2026-07-27 23:09:26 +0530

    Add Footer component to Layout

--- frontend/src/components/Layout/Layout.tsx
+++ frontend/src/components/Layout/Layout.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import Sidebar from './Sidebar';
 import Header from './Header';
+import Footer from './Footer';
 
 export default function Layout() {
   const [sidebarOpen, setSidebarOpen] = useState(true);
@@ -31,10 +32,13 @@ export default function Layout() {
           onMobileMenuClick={() => setMobileSidebarOpen(true)}
           sidebarOpen={sidebarOpen}
         />
-        <main className="flex-1 overflow-y-auto p-4 md:p-6 lg:p-8">
-          <div className="max-w-7xl mx-auto animate-fade-in">
-            <Outlet />
+        <main className="flex-1 overflow-y-auto flex flex-col">
+          <div className="flex-1 p-4 md:p-6 lg:p-8">
+            <div className="max-w-7xl mx-auto animate-fade-in">
+              <Outlet />
+            </div>
           </div>
+          <Footer />
         </main>
       </div>
     </div>
